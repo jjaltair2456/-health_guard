ad9ebf2afea61ceb891f7542693101dd25a9059d
jjaltair2456 <josejimenez2456@gmail.com>
2026-09-15 15:54:22 -0400

    Configurar ruta raiz en urls.py con RedirectView

--- health_guard/urls.py
+++ health_guard/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView  # Importar RedirectView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('records/', include('apps.health_records.urls')),
-    path('', RedirectView.as_view(url='/records/', permanent=False)),  # Redirige la raíz
+    path('', RedirectView.as_view(url='/records/', permanent=False)),  # Redirige la raíz a /records/
 ]
