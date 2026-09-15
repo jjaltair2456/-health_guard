4b682f98e8838cb4b6d5eca68a372cfc2e2baef6
jjaltair2456 <josejimenez2456@gmail.com>
2026-09-15 15:52:28 -0400

    Redirigir ruta raiz a /records/

--- health_guard/urls.py
+++ health_guard/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  # Importar RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Ruta requerida para que el gráfico cargue sus scripts y datos
-    path('django_plotly_dash/', include('django_plotly_dash.urls')),
-    
-    path('authentication/', include('apps.authentication.urls')),
     path('records/', include('apps.health_records.urls')),
-    path('analytics/', include('apps.analytics.urls')),
-    path('users/', include('apps.users.urls')),
+    path('', RedirectView.as_view(url='/records/', permanent=False)),  # Redirige la raíz
 ]
